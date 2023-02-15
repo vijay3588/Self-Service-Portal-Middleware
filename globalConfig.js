a504435f71f8a4501b0d86aa4712161f36a54a12
vijay3588 <blrvijay82@gmail.com>
2023-02-15 11:23:38 +0100

    updated

--- globalConfig.js
+++ globalConfig.js
@@ -2,8 +2,9 @@ module.exports = {
     port: process.env.PORT || "5000",
     publicUrl: process.env.REACT_APP_SSP_MID_PUBLIC_URL || "",
     credKey: process.env.SSP_MID_ENV_CRED,
-    baseURI: process.env.SSP_API_URL|| "https://accept6-front.centralus.cloudapp.azure.com/restapi/api",
     // baseURI: process.env.SSP_API_URL|| "https://accept5-front.centralindia.cloudapp.azure.com/restapi/api",
+    // baseURI: process.env.SSP_API_URL|| "https://accept6-front.centralus.cloudapp.azure.com/restapi/api",
+    baseURI: process.env.SSP_API_URL|| "https://accept5-front.centralindia.cloudapp.azure.com/restapi/api",
 
     databaseURI: process.env.SSP_DB_API_URL || "http://localhost:5001/api",
     logType: "file",
